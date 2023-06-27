f2a5f71bcbdf8363093b4ef221eaa5f41e91d9ea
Emma Scholey <35703828+escholey@users.noreply.github.com>
2023-06-27 11:01:34 +0100

    Remove 2 and 3 parameter dynamic beta
    Models are too complex, can get away with single parameter (lambda) for dynamic beta function

--- helperFunctions/buildForagingModel.m
+++ helperFunctions/buildForagingModel.m
@@ -1,18 +1,16 @@
 function [sim_f, NLL_f, paramsIndex] = buildForagingModel(model, Env, subjData, startValues)
 
 foragingModels = {
-% Model N,       % simulation function                                % NLL% fitting function             % required parameters [alpha patch, alpha rho, beta, dyn_params 1. 2. 3., epsilon]
-    1, @(x)simulate_MVT_avgRR_RW_softmax(x,Env,startValues),   @(x0)NLL_MVT_avgRR_RW_softmax(x0, Env, subjData),   logical([0 1 1 0 0 0 0]);
-    2, @(x)simulate_MVT_patchRR_RW_softmax(x,Env,startValues), @(x0)NLL_MVT_patchRR_RW_softmax(x0, Env, subjData), logical([1 0 1 0 0 0 0]);
-    3, @(x)simulate_MVT_full_RW_softmax(x,Env,startValues),    @(x0)NLL_MVT_full_RW_softmax(x0, Env, subjData),    logical([1 1 1 0 0 0 0]);
-    4, @(x)simulate_MVT_softmax(x,Env),                        @(x0)NLL_MVT_softmax(x0, Env, subjData),            logical([0 0 1 0 0 0 0]);
-    5, @(x)simulate_MVT_patchRR_RW_fixbeta(x,Env,startValues), @(x0)NLL_MVT_patchRR_RW_fixbeta(x0, Env, subjData), logical([1 0 0 0 0 0 0]);
+% Model N,       % simulation function                                % NLL% fitting function             % required parameters [alpha patch, alpha rho, beta, lambda, epsilon]
+    1, @(x)simulate_MVT_avgRR_RW_softmax(x,Env,startValues),   @(x0)NLL_MVT_avgRR_RW_softmax(x0, Env, subjData),   logical([0 1 1 0 0]);
+    2, @(x)simulate_MVT_patchRR_RW_softmax(x,Env,startValues), @(x0)NLL_MVT_patchRR_RW_softmax(x0, Env, subjData), logical([1 0 1 0 0]);
+    3, @(x)simulate_MVT_full_RW_softmax(x,Env,startValues),    @(x0)NLL_MVT_full_RW_softmax(x0, Env, subjData),    logical([1 1 1 0 0]);
+    4, @(x)simulate_MVT_softmax(x,Env),                        @(x0)NLL_MVT_softmax(x0, Env, subjData),            logical([0 0 1 0 0]);
+    5, @(x)simulate_MVT_patchRR_RW_fixbeta(x,Env,startValues), @(x0)NLL_MVT_patchRR_RW_fixbeta(x0, Env, subjData), logical([1 0 0 0 0]);
 
-    6, @(x)simulate_MVT_softmax_dynamicbeta(x,Env,startValues),@(x0)NLL_MVT_softmax_dynamicbeta(x0, Env, subjData),logical([0 0 0 1 0 0 0]);
-    7, @(x)simulate_MVT_softmax_dynamicbeta_twoParams(x,Env,startValues),@(x0)NLL_MVT_softmax_dynamicbeta_twoParams(x0, Env, subjData),logical([0 0 0 1 1 0 0]);
-    8, @(x)simulate_MVT_softmax_dynamicbeta_threeParams(x,Env,startValues),@(x0)NLL_MVT_softmax_dynamicbeta_threeParams(x0, Env, subjData),logical([0 0 0 1 1 1 0]);
+    6, @(x)simulate_MVT_softmax_dynamicbeta(x,Env,startValues),@(x0)NLL_MVT_softmax_dynamicbeta(x0, Env, subjData),logical([0 0 0 1 0]);
 
-    9, @(x)simulate_MVT_e_softmax(x,Env),                      @(x0)NLL_MVT_e_softmax(x0, Env, subjData),            logical([0 0 1 0 0 0 1]);
+    7, @(x)simulate_MVT_e_softmax(x,Env),                      @(x0)NLL_MVT_e_softmax(x0, Env, subjData),          logical([0 0 1 0 1]);
 
 };
 
